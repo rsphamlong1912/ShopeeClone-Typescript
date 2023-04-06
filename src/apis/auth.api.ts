19a8cc0a9316c1e13ecffb98b07ca636f2dad09b
phamlong1912 <rsphamlong1912@gmail.com>
2023-04-06 11:10:11 +0700

    Handle login logic

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,5 +1,6 @@
 import { AuthResponse } from 'src/types/auth.types'
 import http from 'src/utils/http'
 
-export const registerAccount = (body: { email: string; password: string }) =>
-  http.post<AuthResponse>('/registerr', body)
+export const registerAccount = (body: { email: string; password: string }) => http.post<AuthResponse>('/register', body)
+
+export const login = (body: { email: string; password: string }) => http.post<AuthResponse>('/login', body)
