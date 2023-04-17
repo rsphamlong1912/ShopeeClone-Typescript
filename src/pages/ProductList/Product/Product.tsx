394b658ec326ad43862e27fd83d4ae1466ddf56b
phamlong1912 <rsphamlong1912@gmail.com>
2023-04-17 14:54:08 +0700

    UI Product Component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,5 +1,70 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to='/'>
+      <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
+        <div className='relative w-full pt-[100%]'>
+          <img
+            src='https://cf.shopee.vn/file/93b3f6d4799b06f9279f817540daa4d1_tn'
+            alt=''
+            className='absolute top-0 left-0 h-full w-full bg-white object-cover'
+          />
+        </div>
+        <div className='overflow-hidden p-2'>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>
+            [Nhập ELAP5A giảm 5% tối đa 1 triệu] Apple iPhone 14 Pro Max 128GB
+          </div>
+          <div className='mt-3 flex items-center'>
+            <div className='max-w-[50%] truncate text-gray-500 line-through'>₫33.990.000</div>
+            <div className='ml-1 truncate text-orange'>
+              <span className='text-sm'>₫</span>
+              <span>33.990.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='h-3 w-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='h-3 w-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>Đã bán</span>
+              <span className='ml-1'>5.66k</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
